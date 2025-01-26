803cbd27fd77388b2694aebf8ed38a9c18ed357f
fellon-dnb <fellon@ancevt.com>
2025-01-26 15:53:14 +0300

    Initial commit: added basic CRM structure

---
+++ src/test/java/com/example/fellon_crm/FellonCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fellon_crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FellonCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
